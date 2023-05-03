f2929a6b94c0263901a9149692ecea25ffd90634
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-02 23:10:13 -0400

    tag schema

---
+++ server/database/Tag.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema, Types, model } from 'mongoose';
+
+interface Tag {
+  _id?: Types.ObjectId;
+  tagName: string;
+}
+
+const tagSchema = new Schema<Tag>({
+  tagName: { type: String, required: true, unique: true },
+});
+
+export default mongoose.model('Tag', tagSchema);
