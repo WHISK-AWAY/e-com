33ec0f389ef31c79ee01275c2093f3d3ba1429a8
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-03 18:10:45 -0400

    comment out

--- server/database/User.ts
+++ server/database/User.ts
@@ -14,7 +14,9 @@ const cartSchema = new Schema<Cart>(
   {
     products: [
       {
-        product: { type: mongoose.SchemaTypes.ObjectId, ref: 'Product' },
+        product: {
+          product: { type: mongoose.SchemaTypes.ObjectId, ref: 'Product' },
+        },
         qty: Number,
       },
     ],
@@ -37,7 +39,7 @@ const cartSchema = new Schema<Cart>(
   }
 );
 
-interface User {
+export interface User {
   firstName: string;
   lastName: string;
   email: string;
@@ -60,8 +62,8 @@ const userSchema = new Schema<User>({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true, minLength: 8, maxLength: 20 },
   address: {
-    street_1: { type: String, required: true },
-    street_2: { type: String, required: false },
+    address_1: { type: String, required: true },
+    address_2: { type: String, required: false },
     city: { type: String, required: true },
     state: { type: String, required: true },
     zip: { type: String, required: true },
@@ -81,58 +83,58 @@ const userSchema = new Schema<User>({
   },
 });
 
-const generateUser = (count: number): User[] => {
-  const users: User[] = [];
+// const generateUser = (count: number): User[] => {
+//   const users: User[] = [];
 
-  for (let i = 0; i < count; i++) {
-    const firstName = faker.name.firstName();
-    const lastName = faker.name.lastName();
-    const email = faker.internet.email(firstName, lastName);
-    const password = faker.internet.password(8);
-    const address = {
-      address_1: faker.address.streetAddress(),
-      address_2: faker.address.secondaryAddress(),
-      city: faker.address.city(),
-      state: faker.address.stateAbbr(),
-      zip: faker.address.zipCode(),
-    };
-    const favorites = [
-      new Types.ObjectId(),
-      new Types.ObjectId(),
-      new Types.ObjectId(),
-    ];
-    const cart = {
-      products: [
-        {
-          product: {
-            product: new Types.ObjectId(),
-            price: faker.datatype.float({ min: 1, max: 100, precision: 0.01 }),
-          },
-          // price: faker.datatype.float({ min: 1, max: 100, precision: 0.01 }),
-          qty: faker.datatype.number(2),
-        },
-      ],
-    };
-    const role = faker.helpers.arrayElement(['admin', 'user', 'guest']) as
-      | 'admin'
-      | 'user'
-      | 'guest';
+//   for (let i = 0; i < count; i++) {
+//     const firstName = faker.name.firstName();
+//     const lastName = faker.name.lastName();
+//     const email = faker.internet.email(firstName, lastName);
+//     const password = faker.internet.password(8);
+//     const address = {
+//       address_1: faker.address.streetAddress(),
+//       address_2: faker.address.secondaryAddress(),
+//       city: faker.address.city(),
+//       state: faker.address.stateAbbr(),
+//       zip: faker.address.zipCode(),
+//     };
+//     const favorites = [
+//       new Types.ObjectId(),
+//       new Types.ObjectId(),
+//       new Types.ObjectId(),
+//     ];
+//     const cart = {
+//       products: [
+//         {
+//           product: {
+//             product: new Types.ObjectId(),
+//             price: faker.datatype.float({ min: 1, max: 100, precision: 0.01 }),
+//           },
+//           // price: faker.datatype.float({ min: 1, max: 100, precision: 0.01 }),
+//           qty: faker.datatype.number(2),
+//         },
+//       ],
+//     };
+//     const role = faker.helpers.arrayElement(['admin', 'user', 'guest']) as
+//       | 'admin'
+//       | 'user'
+//       | 'guest';
 
-    users.push({
-      firstName,
-      lastName,
-      email,
-      password,
-      address,
-      favorites,
-      cart,
-      role,
-    });
-  }
-  return users;
-};
+//     users.push({
+//       firstName,
+//       lastName,
+//       email,
+//       password,
+//       address,
+//       favorites,
+//       cart,
+//       role,
+//     });
+//   }
+//   return users;
+// };
 
-const mockUsers = generateUser(25);
-console.dir(mockUsers, {depth: 10});
+// const mockUsers = generateUser(25);
+// console.dir(mockUsers, {depth: 10});
 
 export default mongoose.model('User', userSchema);
