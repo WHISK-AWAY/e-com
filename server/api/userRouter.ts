a59914f28e2177afaae6f8478c6e113c4fb319ba
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-06 17:36:25 -0400

    add user router

--- server/api/userRouter.ts
+++ server/api/userRouter.ts
@@ -1,24 +1,50 @@
 import express from 'express';
 const router = express.Router();
 import { User, mongooseConnection } from '../database/index';
-import { requiresAuth, claimCheck } from 'express-openid-connect';
-import { nextTick } from 'process';
+// import { requiresAuth, claimCheck } from 'express-openid-connect';
+// import { nextTick } from 'process';
 // import { jwtCheck, requiresAdmin } from './authMiddleware';
+import {
+  checkAuthenticated,
+  requireAdmin,
+  sameUserOrAdmin,
+} from './authMiddleware';
+import { z, ZodError } from 'zod';
+
+const zodUserId = z.string();
+
+router.get('/', checkAuthenticated, requireAdmin, async (req, res, next) => {
+  try {
+    const allUsers = await User.find({}, '-password');
+    if (!allUsers || allUsers.length === 0)
+      return res.status(404).send('Users do not exist');
+
+    res.status(200).json(allUsers);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get(
-  '/',
-  requiresAuth(), // :````((((
-  // claimCheck((req, claims) => {
-  //   console.log(claims);
-  //   return true;
-  // }),
+  '/:userId',
+  checkAuthenticated,
+  sameUserOrAdmin,
   async (req, res, next) => {
     try {
-      // await mongooseConnection();
-      // const users = await User.find();
-      // const users = await req.oidc.fetchUserInfo();
-      res.status(200).json('send help');
+      const { userId } = req.params;
+      const parsedBody = zodUserId.parse(userId);
+
+      const user = await User.findById(userId, '-password')
+        .populate({ path: 'cart.products.product', populate: { path: 'tags' } })
+        .populate({ path: 'favorites', populate: { path: 'tags' } });
+
+      if (!user)
+        return res.status(404).send('User with the given ID do not exist');
+
+      res.status(200).json(user);
     } catch (err) {
+      if (err instanceof ZodError)
+        return res.status(400).send('Invalid user ID');
       next(err);
     }
   }
