7620e54885bf6488872ad89852e6d8acc53d286d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-06 17:35:56 -0400

    add lign/signup

--- server/api/authRouter.ts
+++ server/api/authRouter.ts
@@ -1,26 +1,93 @@
 import express from 'express';
-import axios from 'axios';
+// import axios from 'axios';
+import { User } from '../database/index';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
 const router = express.Router();
+const SALT_ROUNDS = process.env.SALT_ROUNDS;
+const SECRET = process.env.SECRET;
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { z } from 'zod';
 
-router.get('/login', async (req, res, next) => {
+const zodUser = z
+  .object({
+    firstName: z.string().min(1),
+    lastName: z.string().min(1),
+    email: z.string().email(),
+    password: z.string().min(8).max(20),
+    address: z.object({
+      address_1: z.string(),
+      address_2: z.string().optional(),
+      city: z.string(),
+      state: z.string(),
+      zip: z.string(),
+    }),
+    confirmPassword: z.string().min(8).max(20),
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (password !== confirmPassword) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Password fields do  not match',
+      });
+    }
+  });
+
+router.post('/signup', async (req, res, next) => {
   try {
-    const { data } = await axios.post(
-      'https://dev-z5aj5eewyq3duaqc.us.auth0.com/oauth/token',
-      {
-        client_id: 'eW7bfGKjfMoKlIVRiuev2ehC7Boy5XUd',
-        client_secret:
-          'REDgfT-NsYPiVXrj4kfx4eYw5F0BjaSbpovRfYwK5caX_eVb4_5LwVbqxH-MEiio',
-        audience: 'e-comPB',
-        grant_type: 'client_credentials',
-      },
-      { headers: { 'content-type': 'application/json' } }
-    );
-
-    res.status(200).json(data);
+    const parsedBody = zodUser.parse(req.body);
+    // delete parsedBody.confirmPassword;
+    const newUser = await User.create(parsedBody);
+    res.status(200).json(newUser);
   } catch (err) {
     next(err);
   }
 });
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const userLookup = await User.findOne({ email });
+    if (!userLookup)
+      return res.status(404).send('Cannot find user withn given email');
+    const comparePass = await bcrypt.compare(password, userLookup.password);
+    if (!comparePass)
+      return res.status(403).send('Password does not match database records');
+
+    const token = jwt.sign({ id: userLookup._id, role: userLookup.role }, SECRET!);
+    if (!token) return res.status(500).send('Secret is broken');
+
+    res.status(200).json(token);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+export default router;
+
+// router.get('/login', async (req, res, next) => {
+//   try {
+//     const { data } = await axios.post(
+//       'https://dev-z5aj5eewyq3duaqc.us.auth0.com/oauth/token',
+//       {
+//         client_id: 'eW7bfGKjfMoKlIVRiuev2ehC7Boy5XUd',
+//         client_secret:
+//           'REDgfT-NsYPiVXrj4kfx4eYw5F0BjaSbpovRfYwK5caX_eVb4_5LwVbqxH-MEiio',
+//         audience: 'e-comPB',
+//         grant_type: 'client_credentials',
+//       },
+//       { headers: { 'content-type': 'application/json' } }
+//     );
+
+//     res.status(200).json(data);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
 // router.get('/login', async (req, res, next) => {
 //   try {
 //     const { data } = await axios.post(
@@ -41,4 +108,4 @@ router.get('/login', async (req, res, next) => {
 //   }
 // });
 
-export default router;
+// export default router;
