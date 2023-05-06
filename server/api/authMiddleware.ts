4e0e8f7ebedd7f92c9da6b614f9f9f84a08c602a
Patrick Bryan <pbryan9@gmail.com>
2023-05-05 17:51:05 -0700

    try & fail auth

--- server/api/authMiddleware.ts
+++ server/api/authMiddleware.ts
@@ -1,12 +1,35 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import { auth } from 'express-openid-connect';
 
-import {auth} from 'express-oauth2-jwt-bearer'
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  authorizationParams: {
+    scope: 'openid read:users',
+    audience: 'e-comPB',
+  },
+  secret: '289fahnrga-yas83nfa3ca-',
+  baseURL: 'http://localhost:3001',
+  clientID: '6QeCYaBvy5ZgeovBHHqZYV7WTkix7W2z',
+  issuerBaseURL: 'https://dev-z5aj5eewyq3duaqc.us.auth0.com',
+};
 
+export const auth0config = auth(config);
 
+// import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
 
-export const jwtCheck = auth({
-  audience: 'e-com',
-  issuerBaseURL: 'https://dev-3nurd80vhso7rxtr.us.auth0.com/',
-  tokenSigningAlg: 'RS256',
-});
+// export const jwtCheck = auth({
+//   audience: 'e-comPB',
+//   issuerBaseURL: 'https://dev-z5aj5eewyq3duaqc.us.auth0.com/',
+//   tokenSigningAlg: 'RS256',
+//   secret: '2eah80sd0eansda03enf',
+// });
+
+// export const requiresAdmin = requiredScopes('read:users');
+
+// export const jwtCheck = auth({
+//   audience: 'e-com',
+//   issuerBaseURL: 'https://dev-3nurd80vhso7rxtr.us.auth0.com/',
+//   tokenSigningAlg: 'RS256',
+// });
--- server/api/authRouter.ts
+++ server/api/authRouter.ts
@@ -5,12 +5,12 @@ const router = express.Router();
 router.get('/login', async (req, res, next) => {
   try {
     const { data } = await axios.post(
-      'https://dev-3nurd80vhso7rxtr.us.auth0.com/oauth/token',
+      'https://dev-z5aj5eewyq3duaqc.us.auth0.com/oauth/token',
       {
-        client_id: 'vrf63x8WdTTChYHTEcdP6n79ciqpxPK8',
+        client_id: 'eW7bfGKjfMoKlIVRiuev2ehC7Boy5XUd',
         client_secret:
-          'h_HW63aY0BsKtZasU3Yoju_AXp4NBldAksQ3Lh-q5Kcl2z7sQah1XRbVfBXiwjGo',
-        audience: 'e-com',
+          'REDgfT-NsYPiVXrj4kfx4eYw5F0BjaSbpovRfYwK5caX_eVb4_5LwVbqxH-MEiio',
+        audience: 'e-comPB',
         grant_type: 'client_credentials',
       },
       { headers: { 'content-type': 'application/json' } }
@@ -21,5 +21,24 @@ router.get('/login', async (req, res, next) => {
     next(err);
   }
 });
+// router.get('/login', async (req, res, next) => {
+//   try {
+//     const { data } = await axios.post(
+//       'https://dev-3nurd80vhso7rxtr.us.auth0.com/oauth/token',
+//       {
+//         client_id: 'vrf63x8WdTTChYHTEcdP6n79ciqpxPK8',
+//         client_secret:
+//           'h_HW63aY0BsKtZasU3Yoju_AXp4NBldAksQ3Lh-q5Kcl2z7sQah1XRbVfBXiwjGo',
+//         audience: 'e-com',
+//         grant_type: 'client_credentials',
+//       },
+//       { headers: { 'content-type': 'application/json' } }
+//     );
+
+//     res.status(200).json(data);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 export default router;
