670ad26d3fdb9748130c62f54db1c1f87bc62c5f
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-06 17:34:25 -0400

    add mongoose global connect func

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,10 @@ import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import apiRouter from './api/apiRouter';
+import mongoose from 'mongoose';
 // const { auth } = require('express-oauth2-jwt-bearer');
-dotenv.config();
-import { auth0config } from './api/authMiddleware';
+dotenv.config({ path: '../.env' });
+// import { auth0config } from './api/authMiddleware';
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -15,12 +16,15 @@ const PORT = process.env.PORT || 3002;
 //   issuerBaseURL: 'https://dev-3nurd80vhso7rxtr.us.auth0.com/',
 //   tokenSigningAlg: 'RS256',
 // });
+const init = async () => {
+  await mongoose.connect(process.env.MONGO_DB_URL!);
+};
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(auth0config);
+// app.use(auth0config);
 app.use('/api', apiRouter);
 // app.use(jwtCheck);
 
@@ -41,5 +45,6 @@ app.use('*', (req, res, next) => {
 });
 
 app.listen(PORT, () => {
+  init();
   console.log('Server is listening on port: ' + PORT);
 });
