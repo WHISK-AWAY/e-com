2af2e40796c693f3bb275edcaa17ef797fd4cd30
Patrick Bryan <pbryan9@gmail.com>
2023-05-05 17:51:18 -0700

    try & fail auth

--- server/api/userRouter.ts
+++ server/api/userRouter.ts
@@ -1,25 +1,27 @@
 import express from 'express';
 const router = express.Router();
 import { User, mongooseConnection } from '../database/index';
-import { jwtCheck } from './authMiddleware';
+import { requiresAuth, claimCheck } from 'express-openid-connect';
+import { nextTick } from 'process';
+// import { jwtCheck, requiresAdmin } from './authMiddleware';
 
-
-
-
-
-router.get('/', jwtCheck,  async (req, res, next) => {
-  try {
-    await mongooseConnection();
-    const users = await User.find();
-
-    res.status(200).json(users);
-  } catch (err) {
-    next(err);
+router.get(
+  '/',
+  requiresAuth(), // :````((((
+  // claimCheck((req, claims) => {
+  //   console.log(claims);
+  //   return true;
+  // }),
+  async (req, res, next) => {
+    try {
+      // await mongooseConnection();
+      // const users = await User.find();
+      // const users = await req.oidc.fetchUserInfo();
+      res.status(200).json('send help');
+    } catch (err) {
+      next(err);
+    }
   }
-});
-
-
-
-
+);
 
 export default router;
