aa1e53e0471b989932b4bd10433d9e4d6f329b48
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-09 00:25:44 -0400

    add cart router

--- server/api/cartRouter.ts
+++ server/api/cartRouter.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 const router = express.Router({ mergeParams: true });
-import { User } from '../database/index';
+import { User, Product } from '../database/index';
 import { checkAuthenticated, sameUserOrAdmin } from './authMiddleware';
+import { z } from 'zod';
+
+const zodProduct = z
+  .object({
+    productId: z.string().min(10),
+    qty: z.number().min(1),
+  })
+  .strict();
 
 router.get('/', checkAuthenticated, sameUserOrAdmin, async (req, res, next) => {
   try {
     const { userId } = req.params;
     // console.log('ID', userId);
-    const user = await User.findById(userId, 'cart').populate({path: 'cart.products.product', populate: {path: 
-    'tags'}})
-      if(user === null) return res.status(404).send('User does not exist');
+    const user = await User.findById(userId, 'cart').populate({
+      path: 'cart.products.product',
+      populate: { path: 'tags' },
+    });
+    if (user === null) return res.status(404).send('User does not exist');
 
     res.status(200).json(user);
   } catch (err) {
@@ -17,6 +27,74 @@ router.get('/', checkAuthenticated, sameUserOrAdmin, async (req, res, next) => {
   }
 });
 
+router.post(
+  '/addItem',
+  checkAuthenticated,
+  sameUserOrAdmin,
+  async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+      const user = await User.findById(userId);
+      if (!user) return res.status(404).send('User does not exist');
+      const { productId, qty } = zodProduct.parse(req.body);
+
+      const addItem = await user.cart.addProduct!(productId, qty);
+
+      res.status(201).json(addItem);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  '/removeItem',
+  checkAuthenticated,
+  sameUserOrAdmin,
+  async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+      const user = await User.findById(userId);
+
+      if (!user) return res.status(404).send('User does not exist');
+
+      const { productId, qty } = zodProduct.parse(req.body);
+   
+      const removeItem = await user.cart.removeProduct!!!(productId, qty);
+
+      const minQtyCheck = await User.findById(userId);
+      const insufficientQty = minQtyCheck?.cart.products.filter(
+        (qty) => qty.qty < 1
+      );
+
+      console.log('IQ', insufficientQty);
+      if (insufficientQty?.length! > 0)
+        return res.status(500).send('Insuffient quantity');
+
+      res.status(204).json(removeItem);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  '/clearCart',
+  checkAuthenticated,
+  sameUserOrAdmin,
+  async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+      const user = await User.findById(userId);
+
+      if (!user) return res.status(404).send('User does not exist');
+      const clearCart = await user.cart.clearCart!({ restock: true });
+      res.status(204).json(clearCart);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 
 
