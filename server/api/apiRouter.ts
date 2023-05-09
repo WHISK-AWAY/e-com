cf331d6f69d9effcdc4b8da63d3ec12656835850
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-09 00:25:21 -0400

    add prod rt

--- server/api/apiRouter.ts
+++ server/api/apiRouter.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import userRouter from './userRouter';
 import authRouter from './authRouter';
+import productRouter from './productRouter';
+
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/product', productRouter);
 
 export default router;
