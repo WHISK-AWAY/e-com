ca028cf356abe5e8080a7beb1c69e54fd71545f6
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-10 18:52:28 -0400

    add review rt

--- server/api/reviewRouter.ts
+++ server/api/reviewRouter.ts
@@ -1,19 +1,159 @@
 import express from 'express';
 const router = express.Router({ mergeParams: true });
 import { Product, Review } from '../database/index';
+import { z } from 'zod';
+import mongoose from 'mongoose';
+import { checkAuthenticated, sameUserOrAdmin } from './authMiddleware';
+import { zodReview } from '../../utils';
+
+const zodCreateReview = zodReview.strict();
+const zodUpdateReview = zodReview
+  .strict()
+  .deepPartial()
+  .refine(
+    (data) => {
+      return (
+        data.title !== undefined ||
+        data.content !== undefined ||
+        data.rating !== undefined ||
+        data.nickname !== undefined ||
+        data.location !== undefined
+      );
+    },
+    { message: 'Update at least one of the fields' }
+  );
+
+type TZodReview = z.infer<typeof zodReview>;
+
+const zodProductId = z.string().refine(
+  (productId) => {
+    return mongoose.Types.ObjectId.isValid(productId);
+  },
+  { message: 'Invalid productID' }
+);
+
+const zodReviewId = z.string().refine(
+  (reviewId) => {
+    return mongoose.Types.ObjectId.isValid(reviewId);
+  },
+  {
+    message: 'Invalid reviewID',
+  }
+);
 
 router.get('/', async (req, res, next) => {
   try {
-    const { productId } = req.params as {productId:string};
-    // console.log('prodDI', productId);
-    // console.log('params', req.params);
-    // if(!productId) return res.status()
+    const { productId } = req.params as { productId: string };
+    const validProductId = zodProductId.parse(productId);
 
-    const allReviews = await Review.find({product: productId}).populate({path: 'product', populate: 'tags'});
+    const allReviews = await Review.find({ product: productId }).populate({
+      path: 'product',
+      populate: 'tags',
+    });
     if (!allReviews)
       return res.status(404).send('No reviews available for this product');
 
     res.status(200).json(allReviews);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', checkAuthenticated, async (req, res, next) => {
+  try {
+    const parsedBody: TZodReview & { product?: string; user?: string } =
+      zodCreateReview.parse(req.body);
+
+    const { productId } = req.params;
+    const validProductId = zodProductId.parse(productId);
+    const product = await Product.findById(validProductId);
+    parsedBody.product = productId;
+    parsedBody.user = req.userId;
+    if (!product)
+      return res
+        .status(404)
+        .send('Product you are searching for does not exist');
+
+    const checkExistingReview = await Review.find({
+      product: validProductId,
+      user: req.userId,
+    });
+
+    if (checkExistingReview.length >= 1)
+      return res.status(409).send('Cannot create duplicate review');
+
+    const newReview = await (
+      await Review.create(parsedBody)
+    ).populate(['user', 'product']);
+
+    res.status(201).json(newReview);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:reviewId', checkAuthenticated, async (req, res, next) => {
+  try {
+    const { reviewId } = req.params;
+    const { productId } = req.params;
+    const validReviewId = zodReviewId.parse(reviewId);
+    const validProductId = zodProductId.parse(productId);
+    const userId = req.userId;
+
+    const parsedBody = zodUpdateReview.parse(req.body);
+    const existingReview = await Review.findById(validReviewId);
+
+    if (!existingReview)
+      return res
+        .status(404)
+        .send('Review you are trying to update does not exist');
+    if (
+      String(existingReview.product) !== validProductId ||
+      existingReview.user !== userId
+    )
+      return res.status(400).send('Cannot update review: invalid credentials ');
+
+    if (parsedBody.rating) {
+      if (!parsedBody.rating.overall)
+        parsedBody.rating.overall = existingReview.rating.overall;
+      if (!parsedBody.rating.quality)
+        parsedBody.rating.quality = existingReview.rating.quality;
+      if (!parsedBody.rating.value)
+        parsedBody.rating.value = existingReview.rating.value;
+    }
+
+    const updateReview = await Review.updateOne(
+      { _id: validReviewId },
+      parsedBody
+    );
+
+    res.status(200).json(updateReview);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:reviewId', checkAuthenticated, async (req, res, next) => {
+  try {
+    const { reviewId } = req.params;
+    const userId = req.userId;
+    const validReviewId = zodReviewId.parse(reviewId);
+    const existingReview = await Review.findById(validReviewId);
+    const {productId} = req.params;
+
+    if (!existingReview)
+      return res
+        .status(404)
+        .send('Cannot delete review: it does not exist in the database');
+
+    if (existingReview.user !== userId || String(existingReview.product) !== productId)
+      return res
+        .status(403)
+        .send('Cannot delete review: provided userID or productID does not match');
+
+    await Review.softDelete({ _id: validReviewId });
+
+    res.sendStatus(204);
   } catch (err) {
     next(err);
   }
