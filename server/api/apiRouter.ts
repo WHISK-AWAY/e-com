fbe1a0b266325bd069660db655c02ad6271e3b44
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-12 11:50:20 -0400

    add admin order rt branch

--- server/api/apiRouter.ts
+++ server/api/apiRouter.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import userRouter from './userRouter';
 import authRouter from './authRouter';
 import productRouter from './productRouter';
+import adminOrderRouter from './adminOrderRouter';
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/product', productRouter);
+router.use('/adminOrder', adminOrderRouter);
 
 export default router;
