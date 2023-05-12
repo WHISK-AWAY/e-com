ef1f312e0b67d68bf44f4f087c1e8ac28e9d1c82
Patrick Bryan <pbryan9@gmail.com>
2023-05-12 11:33:36 -0700

    prevent adding 0 qty to cart; other cart tweaks & improvements

--- server/api/cartRouter.ts
+++ server/api/cartRouter.ts
@@ -39,6 +39,10 @@ router.post(
       const { productId, qty } = zodProduct.parse(req.body);
 
       const addItem = await user.cart.addProduct!(productId, qty);
+      if (!addItem)
+        res
+          .status(409)
+          .json({ message: 'Not enough inventory - nothing added to cart' });
 
       res.status(201).json(addItem);
     } catch (err) {
@@ -59,7 +63,7 @@ router.post(
       if (!user) return res.status(404).send('User does not exist');
 
       const { productId, qty } = zodProduct.parse(req.body);
-   
+
       const removeItem = await user.cart.removeProduct!!!(productId, qty);
 
       const minQtyCheck = await User.findById(userId);
@@ -95,7 +99,5 @@ router.post(
     }
   }
 );
-
-
 
 export default router;
