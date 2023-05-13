668b950d658d87b65948ce1a7b6a04d8e27feb5a
Patrick Bryan <pbryan9@gmail.com>
2023-05-13 08:40:57 -0700

    remove user statics section

--- server/database/User.ts
+++ server/database/User.ts
@@ -29,56 +29,41 @@ const cartSchema = new Schema<ICart>(
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
-const userSchema = new Schema<IUser>(
-  {
-    _id: { type: String, default: uuid },
-    firstName: { type: String, required: true, minLength: 2 },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minLength: 8 },
-    address: {
-      address_1: { type: String, required: true },
-      address_2: { type: String, required: false },
-      city: { type: String, required: true },
-      state: { type: String, required: true },
-      zip: { type: String, required: true },
-    },
-    favorites: [
-      {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: 'Product',
-      },
-    ],
-    cart: {
-      type: cartSchema,
-      default: {},
-    },
-    role: {
-      type: String,
-      enum: ['admin', 'user', 'guest'],
-      required: true,
-      default: 'user',
-    },
-    reviewCount: { type: Number, default: 0 },
-    voteCount: { type: Number, default: 0 },
-    skinConcerns: [String],
+const userSchema = new Schema<IUser>({
+  _id: { type: String, default: uuid },
+  firstName: { type: String, required: true, minLength: 2 },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, minLength: 8 },
+  address: {
+    address_1: { type: String, required: true },
+    address_2: { type: String, required: false },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
   },
-  {
-    statics: {
-      // async purgeInactiveCart() {
-      //   const msPerDay = 1000 * 60 * 60 * 24;
-      //   const purgeDay = new Date(Date.now() - msPerDay * 2);
-      //   const allUserCart = await this.find({
-      //     'cart.updatedAt': { $lte: purgeDay },
-      //   });
-      //   if (!allUserCart.length) return;
-      //   for (let user of allUserCart) {
-      //     await user.cart.clearCart!({ restock: true });
-      //   }
-      // },
+  favorites: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Product',
     },
-  }
-);
+  ],
+  cart: {
+    type: cartSchema,
+    default: {},
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user', 'guest'],
+    required: true,
+    default: 'user',
+  },
+  reviewCount: { type: Number, default: 0 },
+  voteCount: { type: Number, default: 0 },
+  skinConcerns: [String],
+});
+
+userSchema.plugin(softDeletePlugin);
 
 userSchema.pre('validate', hashPassword);
 userSchema.pre('updateOne', hashUpdatedPassword);
@@ -101,7 +86,6 @@ userSchema.static('purgeInactiveCart', async function () {
   }
 });
 
-userSchema.plugin(softDeletePlugin);
 export default mongoose.model<
   IUser,
   SoftDeleteModel<IUser> & { purgeInactiveCart: () => void }
